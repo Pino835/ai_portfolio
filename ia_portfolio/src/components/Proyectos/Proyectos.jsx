4e121ea13875726c102740ef5da909725c193ac5
Pino835 <pinoto2002@gmail.com>
2026-05-09 18:25:25 -0600

    Commit #18: - NUEVO CONTENIDO

--- ia_portfolio/src/components/Proyectos/Proyectos.jsx
+++ ia_portfolio/src/components/Proyectos/Proyectos.jsx
@@ -29,6 +29,8 @@ import imagen24 from "../../assets/images/imagen24.jpeg";
 import imagen25 from "../../assets/images/imagen25.png";
 import imagen26 from "../../assets/images/imagen26.png";
 import imagen27 from "../../assets/images/imagen27.png";
+import imagen28 from "../../assets/images/imagen28.png";
+import imagen29 from "../../assets/images/imagen29.png";
 
 // Videos de ejemplo para los proyectos
 import video1 from "../../assets/videos/video1.mp4";
@@ -43,6 +45,7 @@ import video9 from "../../assets/videos/video9.mp4";
 import video10 from "../../assets/videos/video10.mp4";
 import video11 from "../../assets/videos/video11.mp4";
 import video12 from "../../assets/videos/video12.mp4";
+import video13 from "../../assets/videos/video13.mp4";
 
 // Audios de ejemplo para los proyectos
 import audio1 from "../../assets/audios/audio1.mp3";
@@ -50,12 +53,16 @@ import audio2 from "../../assets/audios/audio2.mp3";
 import audio3 from "../../assets/audios/audio3.mp3";
 import audio4 from "../../assets/audios/audio4.mp3";
 import audio5 from "../../assets/audios/audio5.mp3";
+import audio6 from "../../assets/audios/audio6.mp3";
+import audio7 from "../../assets/audios/audio7.mp3";
+import audio8 from "../../assets/audios/audio8.mp3";
 
 // Chatbot de ejemplo para los proyectos
 import chatbot1 from "../../assets/chatbots/chatbot1.png";
 
 // Otras herramientas de ejemplo para los proyectos
 import tool1 from "../../assets/tools/tool1.png";
+import tool2 from "../../assets/tools/tool2.png";
 
 // Datos externos (JSON)
 import proyectosData from "../../data/proyectos.json";
@@ -64,11 +71,11 @@ export default function Proyectos() {
   const secciones = proyectosData.secciones || [];
   const mediaMap = {
     imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11, imagen12, imagen13, imagen14, imagen15,
-    imagen16, imagen17, imagen18, imagen19, imagen20, imagen21, imagen22, imagen23, imagen24, imagen25, imagen26, imagen27, 
-    video1, video2, video3, video4, video5, video6, video7, video8, video9, video10, video11, video12,
-    audio1, audio2, audio3, audio4, audio5,
+    imagen16, imagen17, imagen18, imagen19, imagen20, imagen21, imagen22, imagen23, imagen24, imagen25, imagen26, imagen27, imagen28, imagen29,
+    video1, video2, video3, video4, video5, video6, video7, video8, video9, video10, video11, video12, video13,
+    audio1, audio2, audio3, audio4, audio5, audio6, audio7, audio8,
     chatbot1,
-    tool1
+    tool1, tool2
   };
 
   // Modal state
