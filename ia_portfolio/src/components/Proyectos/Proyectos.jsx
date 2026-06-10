5b209c08af11a13e646212c6da6f67d7a64e4200
Pino835 <pinoto2002@gmail.com>
2026-06-10 17:47:00 -0600

    Commit #23: - CONTENIDO NUEVO - 2 IMAGENES NUEVAS

--- ia_portfolio/src/components/Proyectos/Proyectos.jsx
+++ ia_portfolio/src/components/Proyectos/Proyectos.jsx
@@ -45,6 +45,8 @@ import imagen40 from "../../assets/images/imagen40.png";
 import imagen41 from "../../assets/images/imagen41.png";
 import imagen42 from "../../assets/images/imagen42.png";
 import imagen43 from "../../assets/images/imagen43.png";
+import imagen44 from "../../assets/images/imagen44.png";
+import imagen45 from "../../assets/images/imagen45.png";
 
 // Videos de ejemplo para los proyectos
 import video1 from "../../assets/videos/video1.mp4";
@@ -88,7 +90,7 @@ export default function Proyectos() {
   const mediaMap = {
     imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11, imagen12, imagen13, imagen14, imagen15,
     imagen16, imagen17, imagen18, imagen19, imagen20, imagen21, imagen22, imagen23, imagen24, imagen25, imagen26, imagen27, imagen28, imagen29, imagen30,
-    imagen31, imagen32, imagen33, imagen34, imagen35, imagen36, imagen37, imagen38, imagen39, imagen40, imagen41, imagen42, imagen43,
+    imagen31, imagen32, imagen33, imagen34, imagen35, imagen36, imagen37, imagen38, imagen39, imagen40, imagen41, imagen42, imagen43, imagen44, imagen45,
     video1, video2, video3, video4, video5, video6, video7, video8, video9, video10, video11, video12, video13, video14,
     audio1, audio2, audio3, audio4, audio5, audio6, audio7, audio8,
     chatbot1,
